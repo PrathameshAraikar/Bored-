e55cdf7161b89ddc6a313b7c1b58f07c2a3e9059
Prathamesh Araikar <prathamesharaikar11@gmail.com>
2022-01-23 19:17:43 +0530

    Initial Commit

---
+++ Bored??/Bored__App.swift
@@ -0,0 +1,17 @@
+//
+//  Bored__App.swift
+//  Bored??
+//
+//  Created by Prathamesh Araikar on 23/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Bored__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
