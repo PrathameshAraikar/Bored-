f19edf552dff31357083bd146040c4fa6971857e
Prathamesh Araikar <prathamesharaikar11@gmail.com>
2023-02-24 10:58:57 +0530

    Bored Commit

---
+++ Bored??/FinalView.swift
@@ -0,0 +1,20 @@
+//
+//  FinalView.swift
+//  Bored??
+//
+//  Created by Prathamesh Araikar on 26/01/22.
+//
+
+import SwiftUI
+
+struct FinalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FinalView_Previews: PreviewProvider {
+    static var previews: some View {
+        FinalView()
+    }
+}
